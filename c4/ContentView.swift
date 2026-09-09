95d1fea18034b9c0c1b409f2fcee3689bf642d12
kuyk15820-cpu <kuyk15820@gmail.com>
2026-09-10 00:12:11 +0700

    Update ContentView.swift

--- c4/ContentView.swift
+++ c4/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
     }
 
     var body: some View {
-        TargetGameView()
+        MainContainerView()
             .tint(AppTheme.accent)
             .imageScale(.small)
             .onChange(of: patchDraftCoordinator.request?.id) { requestID in
