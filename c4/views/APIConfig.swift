8eb548457407a803c6f578d7c837588eea956f71
kuyk15820-cpu <kuyk15820@gmail.com>
2026-09-10 21:58:03 +0700

    Create APIConfig.swift

---
+++ c4/views/APIConfig.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum APIConfig {
+    // 🟢 เปลี่ยน Domain ให้เป็นของคุณ (แนะนำให้ใช้ https://)
+    static let baseURL = "https://f1x3r/api/webserver/api/connect.php"
+    
+    // 🟢 ใส่ Token ของ Package ที่ได้จาก Admin Dashboard
+    static let packageToken = "112657011667e7acf2afaf96d464af51"
+}
