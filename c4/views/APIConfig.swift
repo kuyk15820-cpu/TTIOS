6e7fa6c05c2243fed73648a7aa01e983a2db9e48
kuyk15820-cpu <kuyk15820@gmail.com>
2026-09-10 22:42:36 +0700

    Update APIConfig.swift

--- c4/views/APIConfig.swift
+++ c4/views/APIConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum APIConfig {
     // 🟢 เปลี่ยน Domain ให้เป็นของคุณ (แนะนำให้ใช้ https://)
-    static let baseURL = "https://f1x3r/api/webserver/api/connect.php"
+    static let baseURL = "https://f1x3r.org/api/webserver/api/connect.php"
     
     // 🟢 ใส่ Token ของ Package ที่ได้จาก Admin Dashboard
     static let packageToken = "112657011667e7acf2afaf96d464af51"
