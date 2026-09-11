19d2a7aba1652d1750067041962858f639802bd1
kuyk15820-cpu <kuyk15820@gmail.com>
2026-09-11 09:03:49 +0700

    Update APIConfig.swift

--- c4/views/APIConfig.swift
+++ c4/views/APIConfig.swift
@@ -5,5 +5,5 @@ enum APIConfig {
     static let baseURL = "https://f1x3r.org/api/webserver/api/connect.php"
     
     // 🟢 ใส่ Token ของ Package ที่ได้จาก Admin Dashboard
-    static let packageToken = "112657011667e7acf2afaf96d464af51"
+    static let packageToken = "c4-f1df58fa49586acb0cd33859"
 }
